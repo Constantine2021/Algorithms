65fb38abdfec64c818130cf19b700db919e01f79
Constantine2021 <kosta2016@icloud.com>
2021-11-18 22:48:10 +0300

    opt

--- src/com/company/sort/StringSort.java
+++ src/com/company/sort/StringSort.java
@@ -5,39 +5,41 @@ public class StringSort {
     public static void main(String[] args) {
         System.out.println("Array of strings: ");
         String[] strArray = new String[]{"zvnmb", "jlkoph", "muywasd", "abcbb", "abcbbb", "abc", "muy" ,"aaaaaaaa", "afc"};
-        for (int i = 0; i < strArray.length; i++) {
-            System.out.println(strArray[i]);
+        for (String i : strArray) {
+            System.out.println(i);
         }
 
         for (int i = strArray.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (strArray[j].compareTo(strArray[j+1]) > 0) {
                     String temp = strArray[j];
-                    strArray[j] = strArray[j].replaceAll(strArray[j], strArray[j+1]);
-                    strArray[j+1] = strArray[j+1].replaceAll(strArray[j+1], temp);
+                    strArray[j] = strArray[j+1];
+                    strArray[j+1] = temp;
                 }
 //                if (strArray[j].charAt(0) == strArray[j + 1].charAt(0)) {
 //                    int len = Math.min(strArray[j].length(),strArray[j+1].length());
 //                    for (int k = 1; k < len; k++) {
 //                        if (strArray[j].charAt(k) > strArray[j + 1].charAt(k)) {
-//                            String temp = strArray[j];
-//                            strArray[j] = strArray[j].replaceAll(strArray[j], strArray[j+1]);
-//                            strArray[j+1] = strArray[j+1].replaceAll(strArray[j+1], temp);
+//                            strArray[j] = strArray[j] + strArray[j+1];
+//                            strArray[j + 1] = strArray[j].substring(0,
+//                                    strArray[j].length() - strArray[j+1].length());
+//                            strArray[j] = strArray[j].substring(strArray[j+1].length());
 //                            break;
 //                        }
 //                    }
 //                }
 //                else if (strArray[j].charAt(0) > strArray[j + 1].charAt(0)) {
-//                    String temp = strArray[j];
-//                    strArray[j] = strArray[j].replaceAll(strArray[j], strArray[j+1]);
-//                    strArray[j+1] = strArray[j+1].replaceAll(strArray[j+1], temp);
+//                    strArray[j] = strArray[j] + strArray[j+1];
+//                    strArray[j + 1] = strArray[j].substring(0,
+//                            strArray[j].length() - strArray[j+1].length());
+//                    strArray[j] = strArray[j].substring(strArray[j+1].length());
 //                }
             }
         }
 
         System.out.println("===============Sort=================================");
-        for (int i = 0; i < strArray.length; i++) {
-            System.out.println(strArray[i]);
+        for (String i : strArray) {
+            System.out.println(i);
         }
     }
 }
