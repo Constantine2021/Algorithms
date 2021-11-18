b0af4c118244b369743f4a3427b46fa395067293
Constantine2021 <kosta2016@icloud.com>
2021-11-18 22:55:29 +0300

    opt2

--- src/com/company/sort/StringSort.java
+++ src/com/company/sort/StringSort.java
@@ -2,9 +2,18 @@ package com.company.sort;
 
 
 public class StringSort {
+
+    static String[] strArray = new String[]{"zvnmb", "jlkoph", "muywasd", "abcbb",
+            "abcbbb", "abc", "muy" ,"aaaaaaaa", "afc"};
+
+    static void stringsSwap(int j) {
+        String temp = strArray[j];
+        strArray[j] = strArray[j+1];
+        strArray[j+1] = temp;
+    }
+
     public static void main(String[] args) {
         System.out.println("Array of strings: ");
-        String[] strArray = new String[]{"zvnmb", "jlkoph", "muywasd", "abcbb", "abcbbb", "abc", "muy" ,"aaaaaaaa", "afc"};
         for (String i : strArray) {
             System.out.println(i);
         }
@@ -12,9 +21,7 @@ public class StringSort {
         for (int i = strArray.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (strArray[j].compareTo(strArray[j+1]) > 0) {
-                    String temp = strArray[j];
-                    strArray[j] = strArray[j+1];
-                    strArray[j+1] = temp;
+                    stringsSwap(j);
                 }
 //                if (strArray[j].charAt(0) == strArray[j + 1].charAt(0)) {
 //                    int len = Math.min(strArray[j].length(),strArray[j+1].length());
