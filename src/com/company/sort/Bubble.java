a2acb4411b9539587e8e687ed9da1be57332461d
Constantine2021 <kosta2016@icloud.com>
2022-02-02 00:11:44 +0300

    improve Bubble

--- src/com/company/sort/Bubble.java
+++ src/com/company/sort/Bubble.java
@@ -18,21 +18,37 @@ public class Bubble {
         }
         scanner.close();
 
+        int[] arr = new int[]{1,2,3,4,5,65};
+        int lenArr = arr.length;
+        
         System.out.println("===============Sort=================================");
-        for (int i = len - 1; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (randArray[j] > randArray[j + 1]) {
-                    int temp = randArray[j + 1];
-                    randArray[j + 1] = randArray[j];
-                    randArray[j] = temp;
-                }
-            }
-        }
-
+        bubbleSort(len, randArray);
+        bubbleSort(lenArr, arr);
+        
         for (int i = 0; i < len; i++) {
             System.out.print(randArray[i] + " ");
             if ((i + 1) % 10 == 0)
                 System.out.println();
+        }
+        System.out.println("====================================================");
+        for (int i: arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void bubbleSort(int length, int[] array) {
+        int numOfSwap = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    ++numOfSwap;
+                }
+            }
+            if (numOfSwap == 0)
+                break;
         }
     }
 }
